9720ec8d502f06633011f961198a361826a71b56
OnurBaha <koconurbaha@gmail.com>
2024-11-27 11:52:04 +0300

    Add project files.

---
+++ DataAccess/Contexts/BookShelfContext.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace DataAccess.Contexts;
+
+public class BookShelfContext : DbContext
+{
+    protected IConfiguration Configuration { get; set; }
+
+    public DbSet<OperationClaim> OperationClaims { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }  
+    public DbSet<User> Users { get; set; }
+
+    public BookShelfContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+    {
+        Configuration = configuration;
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ DataAccess/DataAccessServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess.Abstracts;
+using DataAccess.Concretes;
+using DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess;
+
+public static class DataAccessServiceRegistration
+{
+    public static IServiceCollection AddDataAccessServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BookShelfContext>(options => options.UseSqlServer(configuration.GetConnectionString("BookShelfContext")));
+
+      
+        services.AddScoped<IUserDal, EfUserDal>();
+       
+        services.AddScoped<IOperationClaimDal, EfOperationClaimDal>();
+        services.AddScoped<IUserOperationClaimDal, EfUserOperationClaimDal>();
+        services.AddScoped<IOperationClaimDal, EfOperationClaimDal>();
+
+
+
+        return services;
+    }
+}
